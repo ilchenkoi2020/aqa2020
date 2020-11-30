53ef39ce9610e774f528b0596f8f2f472e9ca0a8
IlchenkoIvan <ivan.ilchenko@globallogic.com>
2020-11-30 18:00:08 +0200

    ui

--- src/test/java/test/UiTest.java
+++ src/test/java/test/UiTest.java
@@ -2,6 +2,7 @@ package test;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -11,12 +12,34 @@ import java.net.URL;
 
 public class UiTest {
     @Test(groups = {"ui", "full"})
-    public void getGuru99Page() throws MalformedURLException {
+    public void chromeDriver() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName("chrome");
-        capabilities.setPlatform(Platform.LINUX);
+        capabilities.setPlatform(Platform.MAC);
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless"); //should be enabled for Jenkins
+//        options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
+//        options.addArguments("--window-size=1920x1080"); //should be enable
         WebDriver driver;
         driver = new RemoteWebDriver(new URL("http://34.76.237.86:4444/wd/hub"), capabilities);
+//        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        driver.get("https://www.guru99.com/introduction-to-selenium-grid.html");
+        driver.quit();
+    }
+
+    @Test(groups = {"ui", "full"})
+    public void remoteWebDriver() {
+        WebDriver driver = new ChromeDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setPlatform(Platform.MAC);
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless"); //should be enabled for Jenkins
+//        options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
+//        options.addArguments("--window-size=1920x1080"); //should be enable
+//        WebDriver driver;
+//        driver = new RemoteWebDriver(new URL("http://34.76.237.86:4444/wd/hub"), capabilities);
+//        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
         driver.get("https://www.guru99.com/introduction-to-selenium-grid.html");
         driver.quit();
     }
