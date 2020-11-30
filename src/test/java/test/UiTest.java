800687da827c187d1512fa219dd5be9182fc3b84
IlchenkoIvan <ivan.ilchenko@globallogic.com>
2020-11-30 19:03:32 +0200

    ui

--- src/test/java/test/UiTest.java
+++ src/test/java/test/UiTest.java
@@ -3,6 +3,7 @@ package test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -11,26 +12,30 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class UiTest {
-//    @Test(groups = {"ui", "full"})
-//    public void chromeDriver() throws MalformedURLException {
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setBrowserName("chrome");
-//        capabilities.setPlatform(Platform.LINUX);
-////        options.addArguments("--no-sandbox");
-////        options.addArguments("--headless"); //should be enabled for Jenkins
-////        options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
-////        options.addArguments("--window-size=1920x1080"); //should be enable
-//        WebDriver driver;
-//        driver = new RemoteWebDriver(new URL("http://34.76.237.86:4444/wd/hub"), capabilities);
-////        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-//        driver.get("https://www.guru99.com/introduction-to-selenium-grid.html");
-//        driver.quit();
-//    }
+    @Test(groups = {"ui", "full"})
+    public void chromeDriver() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setPlatform(Platform.LINUX);
+        capabilities.setCapability("headless", true);
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless"); //should be enabled for Jenkins
+//        options.addArguments("--disable-dev-shm-usage"); //should be enabled for Jenkins
+//        options.addArguments("--window-size=1920x1080"); //should be enable
+        WebDriver driver;
+        driver = new RemoteWebDriver(new URL("http://34.76.237.86:4444/wd/hub"), capabilities);
+//        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        driver.get("https://www.guru99.com/introduction-to-selenium-grid.html");
+        driver.quit();
+    }
 
     @Test(groups = {"ui", "full"})
     public void testGoogleSearch() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/home/m2b011990/selenium/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+        WebDriver driver = new ChromeDriver(options);
+//        WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com/");
         Thread.sleep(5000);  // Let the user actually see something!
         Thread.sleep(5000);  // Let the user actually see something!
